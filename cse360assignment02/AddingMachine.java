a704a3dd9cd0fdc39a6967fbd81f26372947b4af
rkang5 <rkang5@asu.edu>
2020-10-06 20:07:28 -0700

    addingmachine version 2

--- cse360assignment02/AddingMachine.java
+++ cse360assignment02/AddingMachine.java
@@ -1,28 +1,34 @@
-/* * The HelloWorld program implements an application that * simply displays "Hello World!" to the standard output. * <p> * Giving proper comments in your program makes it more * user friendly and it is assumed as a high quality code. *  * * @author  Rachel * @version 1.0 * @since   2020-09-03  */
-
 package cse360assignment02;
 
 public class AddingMachine {
   private int total;
+  StringBuffer history;
   
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
+    history = new StringBuffer("0");
   }
   
   public int getTotal () {
-    return 0;
+    return total;
   }
   
   public void add (int value) {
+    total = total + value;
+    history.append("+"+value);
   }
 
   public void subtract (int value) {
+    total = total - value;
+    history.append("-"+value);
   }
 
   public String toString () {
-    return "";
+    return history.toString();
   }
 
   public void clear() {
+    total = 0;
+    history.delete(0, history.length());
   }
 }
